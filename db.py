aba006184a9eb818e03f0b060af0dd2681bc8acb
japroc <japroc@mail.ru>
2018-08-31 20:44:01 -0400

    [ADD][MALL] management features

--- db.py
+++ db.py
@@ -39,6 +39,20 @@ class CombotMall(Base):
     description = Column(String, nullable=False)
     create_time = Column(DateTime, nullable=False, default=datetime.datetime.now)
 
+def get_all_entries():
+    session = new_session()
+    entries = session.query(CombotMall).all()
+    session.close()
+    for entry in entries:
+        print('{}:@{}:{}:{}'.format(entry.id, entry.seller_username, 
+                                    entry.description, entry.create_time))
+
+def delete_by_id(id):
+    session = new_session()
+    session.query(CombotMall).filter(CombotMall.id == id).delete()
+    session.commit()
+    session.close()
+
 # -------------
 # --- Utils ---
 # -------------
