221c21fe05de90380a9cdbbd714c2b0cc4363f1c
japroc <japroc@mail.ru>
2018-11-11 17:32:22 -0500

    [API] added all api function to class API

--- app/api.py
+++ app/api.py
@@ -33,3 +33,89 @@ class API:
 
         # return
         return res
+
+    @staticmethod
+    def pinMsg(chat_id, msg_id):
+
+        # prepare payload
+        payload = {
+            'chat_id': chat_id,
+            'message_id': msg_id
+        }
+
+        # make request
+        r = requests.get(BASE_URL + 'pinChatMessage', params=payload)
+        data = r.json()
+
+        # return
+        return data['result'] if data['ok'] is True else None
+
+    @staticmethod
+    def sendMsgAndPin(chat_id, text):
+
+        # send msg
+        msg = API.apiSendMsg(chat_id, text)
+        if msg is None:
+            return None
+
+        # pin msg
+        API.apiPinMsg(chat_id, msg['message_id'])
+
+        # return
+        return msg
+
+    @staticmethod
+    def sendPhoto(chat_id, photo_url, caption=None, explicit_return=False):
+
+        # prepare payload
+        payload = {
+            'chat_id': chat_id,
+            'photo': photo_url
+        }
+        if caption:
+            payload['caption'] = caption
+
+        # make request
+        r = requests.get(BASE_URL + 'sendPhoto', params=payload)
+        data = r.json()
+
+        # prepare result
+        res = None
+        if explicit_return:
+            res = data
+        elif data['ok'] is True:
+            res = data['result']
+
+        return res
+
+    @staticmethod
+    def forwardMsg(from_chat_id, to_chat_id, msg_id):
+
+        # prepare payload
+        payload = {
+            'chat_id': to_chat_id,
+            'from_chat_id': from_chat_id,
+            'message_id': msg_id
+        }
+
+        # make request
+        r = requests.get(BASE_URL + 'forwardMessage', params=payload)
+        data = r.json()
+
+        # return
+        return data['result'] if data['ok'] is True else None
+
+    @staticmethod
+    def unpinMsg(chat_id):
+
+        # prepare payload
+        payload = {
+            'chat_id': chat_id,
+        }
+        
+        # make request
+        r = requests.get(BASE_URL + 'unpinChatMessage', params=payload)
+        data = r.json()
+
+        # return
+        return data['result'] if data['ok'] is True else None
