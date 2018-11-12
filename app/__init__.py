129a6a05e8ab69248fddcc9084c74e6b18b0cf67
japroc <japroc@mail.ru>
2018-11-12 13:26:50 -0500

    Removed redundant hw code

--- app/__init__.py
+++ app/__init__.py
@@ -132,51 +132,6 @@ def handlePing(msg):
     API.sendMsg(chat_id, text)
     # print('[+] handlePing')
 
-# -----------
-# --- /hw ---
-# -----------
-
-def hwHandle_response(msg):
-
-    HW = [
-        'делаете', 'пидоры то такие', 'не учитесь',
-        'бухаете', 'пишите', 'творите', 'кушаете',
-        'не спите', 'можете?', 'имеете против меня?',
-        'такие динозавры', 'опять спамите эту хуйню',
-        'курите?', 'заебываете меня!!', '?? не пойти ли вам н@&yi?'
-        'такие дерзские?', 'комбатов не боитесь', 'выселения не боитесь',
-        'отчисления не боитесь', 'удивляетесь', 'доебались',
-        'приперлись, вас никто не ждал здесь',
-        '? почему вы ? да идите нахуй просто! Я бот, мне можно все.',
-        'понтуетесь?', 'шумите!!!', 'хотели-то?'
-    ]
-
-    resp_msg = 'Huli Wi {}'.format(random.choice(HW))
-    API.sendMsg(msg['chat']['id'], resp_msg)
-
-def hwHandle_body(msg):
-
-    candidates = [
-        ', бро, хватит уже', ', бро, заебал',
-        ', бро, заебал с этой херней', ', заебал, реально',
-        ', прекрати', ', твоюжмать, хватит уже!'
-    ]
-
-    _from = msg['from']
-    name = _from['first_name'] or _from['username'] or _from['last_name'] or 'Эй'
-
-    resp_msg = name + random.choice(candidates)
-    API.sendMsg(msg['chat']['id'], resp_msg)
-
-def hwHandle(msg):
-    if random.randint(0, 1) != 0:
-        return
-    if random.randint(0, 1) == 0:
-        hwHandle_body(msg)
-    else:
-        hwHandle_response(msg)
-
-
 # ----------------------
 # --- ADMIN COMMANDS ---
 # ----------------------
